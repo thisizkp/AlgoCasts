5139c2d9f700d0c8cf53bb0f3a227329d5516015
Prasanth Karri <prasanth.25k@gmail.com>
2018-05-29 18:30:05 +0530

    solution: string reverse

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,32 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+
+function reverse(str) {
+  const reverse_reducer = (accumulator, currentValue) => currentValue + accumulator;
+  return str.split('').reduce(reverse_reducer, "");
+}
 
 module.exports = reverse;
+
+// function reverse(str) {
+//   return str.split('').reverse().join('');
+// }
+
+// function reverse(str) {
+//   let reverse_st = "";
+//   const str_len = str.length;
+
+//   for(let i=str_len-1; i>=0; i--) {
+//     reverse_st += str[i];
+//   }
+//   return reverse_st;
+// }
+
+// function reverse(str) {
+//   let reversed = "";
+//   for(let character of str) {
+//     reversed = character + reversed;
+//   }
+//   return reversed;
+// }
