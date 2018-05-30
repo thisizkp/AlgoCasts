870001c0c95b0b20d1ec19d5cf9f962c252983cc
Prasanth Karri <prasanth.25k@gmail.com>
2018-05-30 16:09:00 +0530

    solution: reverse integer

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,18 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  let rev = 0;
+  while (Math.abs(n) > 0) {
+    rev = rev * 10 + n%10;
+    n = Math.trunc(n/10);
+  }
+  return n >= 0 ? rev : -rev;
+}
 
 module.exports = reverseInt;
+
+// function reverseInt(n) {
+//   const rev = String(n).split('').reverse().join('');
+//   return n >= 0 ? Number(rev) : Number(`-${rev.substr(0, rev.length-1)}`); 
+// }
